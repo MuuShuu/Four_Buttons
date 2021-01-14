719b63aee6b85c331a55c8dbf4c5a788988c75e4
MuuShuu <MuuShuu@users.noreply.github.com>
2021-01-14 22:32:47 +0200

    1

--- four.js
+++ four.js
@@ -1,126 +1,26 @@
-var btnBlack = document.querySelector('#btnBlack');
-        
-    btnBlack.addEventListener('click', function(){
+const buttons = document.getElementById('btns');
 
-        const bigSquare = document.querySelector('.black.square-100')
-
-        if (bigSquare) {
-        
-            bigSquare.parentNode.removeChild(bigSquare)
+function getColor(event){
+    if (event.target.tagName !== 'BUTTON') return;
+    const btnColor = event.target.dataset.color
+    const bigSquare = document.querySelector('.' + btnColor + '.square-100')
     
+    if (bigSquare) {
+        bigSquare.parentNode.removeChild(bigSquare)
+    } else {
+        const smallSquare = document.querySelector('.' + btnColor + '.square-50')
+
+        if (smallSquare) {
+            smallSquare.classList.remove('square-50')
+            smallSquare.classList.add('square-100')
         } else {
-
-            const smallSquare = document.querySelector('.black.square-50')
-
-            if (smallSquare) {
-
-                smallSquare.classList.remove('square-50')
-                smallSquare.classList.add('square-100')
-
-            } else {
-
-                const newElement = document.createElement('div');
-        
-                newElement.classList.add('black')
-                newElement.classList.add('square-50')
-    
-                document.body.appendChild(newElement);
-            }
+            const newElement = document.createElement('div');  
+            newElement.classList.add(btnColor)
+            newElement.classList.add('square-50')
+            document.body.appendChild(newElement);
         }
-    })
+    }
+}
 
-
-var btnRed = document.querySelector('#btnRed');
-        
-    btnRed.addEventListener('click', function(){
-
-        const bigSquere = document.querySelector('.red.square-100')
-
-        if (bigSquere) {
-        
-            bigSquere.parentNode.removeChild(bigSquere)
-    
-        } else {
-
-            const smallSquere = document.querySelector('.red.square-50')
-
-            if (smallSquere) {
-
-                smallSquere.classList.remove('square-50')
-                smallSquere.classList.add('square-100')
-
-            } else {
-
-                const newElement = document.createElement('div');
-        
-                newElement.classList.add('square-50');
-                newElement.classList.add('red');
-    
-                document.body.appendChild(newElement);
-            }
-        }
-    })
-
-
-var btnBlue = document.querySelector('#btnBlue');
-        
-    btnBlue.addEventListener('click', function(){
-
-        const bigSquere = document.querySelector('.blue.square-100')
-
-        if (bigSquere) {
-        
-            bigSquere.parentNode.removeChild(bigSquere)
-    
-        } else {
-
-            const smallSquere = document.querySelector('.blue.square-50')
-
-            if (smallSquere) {
-
-                smallSquere.classList.remove('square-50')
-                smallSquere.classList.add('square-100')
-
-            } else {
-
-                const newElement = document.createElement('div');
-        
-                newElement.classList.add('square-50')
-                newElement.classList.add('blue');
-    
-                document.body.appendChild(newElement);
-            }
-        }
-    })
-
-
-var btnGreen = document.querySelector('#btnGreen');
-        
-    btnGreen.addEventListener('click', function(){
-
-        const bigSquere = document.querySelector('.green.square-100')
-
-        if (bigSquere) {
-        
-            bigSquere.parentNode.removeChild(bigSquere)
-    
-        } else {
-
-            const smallSquere = document.querySelector('.green.square-50')
-
-            if (smallSquere) {
-
-                smallSquere.classList.remove('square-50')
-                smallSquere.classList.add('square-100')
-
-            } else {
-
-                const newElement = document.createElement('div');
-        
-                newElement.classList.add('square-50')
-                newElement.classList.add('green');
-    
-                document.body.appendChild(newElement);
-            }
-        }
-    })
+buttons.addEventListener('click', getColor);
+       
